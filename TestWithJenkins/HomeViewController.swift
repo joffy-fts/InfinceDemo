6107ef55dd6a04b1e9f1d8fa8c53ccc55419af28
Archana M <archana.m@fingent.com>
2019-01-02 17:13:25 +0530

    2nd commit

--- TestWithJenkins/HomeViewController.swift
+++ TestWithJenkins/HomeViewController.swift
@@ -27,6 +27,8 @@ class HomeViewController: UIViewController {
             nsDictionary = NSDictionary(contentsOfFile: path)
             
             print("plist data == ",nsDictionary)
+            let colorVal = nsDictionary!["app_color"] as! String
+            self.view.backgroundColor = colorWithHexString(hex: colorVal)
         }
         // Do any additional setup after loading the view.
     }
@@ -40,15 +42,29 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func colorWithHexString (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: CharacterSet.whitespaces)
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substring(from: 1)
+        }
+        
+        if (cString.characters.count != 6) {
+            return UIColor.gray
+        }
+        
+        let rString = (cString as NSString).substring(to: 2)
+        let gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
+        let bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+        Scanner(string: rString).scanHexInt32(&r)
+        Scanner(string: gString).scanHexInt32(&g)
+        Scanner(string: bString).scanHexInt32(&b)
+        
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
     }
-    */
-
+    
 }
